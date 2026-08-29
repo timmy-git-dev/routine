777585c2256c658651e5e70dd48f2ee2dfdc32ed
timmy-git-dev <timmy.mail.dev@proton.me>
2026-08-28 20:37:28 -0600

    wip menu system (very broken), updating time system due to lack of day/month handling.

--- inc/Time.hpp
+++ inc/Time.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <ctime>
 #include <tuple>
 struct Time
 {
+    // unsigned long date;
+
     int year;
     int month;
     int day;
@@ -12,4 +15,36 @@ struct Time
     bool operator> (const Time& _time) const {return std::tie(year, month, day, hour, minute) >  std::tie(_time.year, _time.month, _time.day, _time.hour, _time.minute);}
     bool operator< (const Time& _time) const {return std::tie(year, month, day, hour, minute) <  std::tie(_time.year, _time.month, _time.day, _time.hour, _time.minute);}
     bool operator==(const Time& _time) const {return std::tie(year, month, day, hour, minute) == std::tie(_time.year, _time.month, _time.day, _time.hour, _time.minute);}
+
+    void update()
+    {
+        hour   +=  minute < 0 ? (minute - 59) / 60 : minute / 60;
+        minute -= (minute < 0 ? (minute - 59) / 60 : minute / 60) * 60;
+
+        day    +=  hour   < 0 ? (hour   - 23) / 24 : hour   / 24;
+        hour   -= (hour   < 0 ? (hour   - 23) / 24 : hour   / 24) * 24;
+
+        month  +=  day    < 0 ? (day    - 30) / 31 : day    / 31;
+        day    -= (day    < 0 ? (day    - 30) / 31 : day    / 31) * 31;
+
+        year   +=  month  < 0 ? (month  - 11) / 12 : month  / 12;
+        month  -= (month  < 0 ? (month  - 11) / 12 : month  / 12) * 12;
+    }
+
+
+    static Time now()
+    {
+        std::time_t _now = std::time(nullptr);
+        std::tm _local{};
+        localtime_r(&_now, &_local);
+
+        return Time
+        {
+            .year   = _local.tm_year + 1900,
+            .month  = _local.tm_mon  + 1,
+            .day    = _local.tm_mday,
+            .hour   = _local.tm_hour,
+            .minute = _local.tm_min,
+        };
+    }
 };
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,7 +1,10 @@
 #include "Menu.hpp"
+#include "Task.hpp"
 #include "Terminal.hpp"
 #include "Input.hpp"
+#include "Time.hpp"
 #include <cstddef>
+#include <vector>
 
 namespace menu
 {
@@ -15,8 +18,7 @@ namespace menu
             "STATUS:      {}\n"
             "SUBTASKS:    {}\n"
             "START:       {:04}/{:02}/{:02} {:02}:{:02}\n"
-            "END:         {:04}/{:02}/{:02} {:02}:{:02}\n"
-            "---\n",
+            "END:         {:04}/{:02}/{:02} {:02}:{:02}\n",
             _task.name,
             _task.description,
             _task.status,
@@ -72,9 +74,137 @@ namespace menu
             }
         }
     }
-    void new_task(Task &_task)
+
+    enum class NEW_STATE : int
     {
-        // ...
+        CANCEL  = -1,
+
+        NAME        ,
+        DESCRIPTION ,
+        START_YEAR  ,
+        START_MONTH ,
+        START_DAY   ,
+        START_HOUR  ,
+        START_MINUTE,
+        END_YEAR    ,
+        END_MONTH   ,
+        END_DAY     ,
+        END_HOUR    ,
+        END_MINUTE  ,
+
+        CONFIRM,
+    };
+    enum class MENU_CASE : int
+    {
+        BACK = -1,
+        STAY =  0,
+        NEXT =  1,
+    };
+
+    void print_new_task(const Task &_task, const NEW_STATE _state)
+    {
+        terminal::print
+        (
+            "%^"
+            "NAME:        {}\n"
+            "DESCRIPTION: {}\n"
+            "START:       {:04}/{:02}/{:02} {:02}:{:02}\n"
+            "END:         {:04}/{:02}/{:02} {:02}:{:02}\n",
+            _state == NEW_STATE::NAME         ? "%^%*" : "%^", _task.name            ,
+            _state == NEW_STATE::DESCRIPTION  ? "%^%*" : "%^", _task.description     ,
+
+            _state == NEW_STATE::START_YEAR   ? "%^%*" : "%^", _task.startTime.year  ,
+            _state == NEW_STATE::START_MONTH  ? "%^%*" : "%^", _task.startTime.month ,
+            _state == NEW_STATE::START_DAY    ? "%^%*" : "%^", _task.startTime.day   ,
+            _state == NEW_STATE::START_HOUR   ? "%^%*" : "%^", _task.startTime.hour  ,
+            _state == NEW_STATE::START_MINUTE ? "%^%*" : "%^", _task.startTime.minute,
+
+            _state == NEW_STATE::END_YEAR     ? "%^%*" : "%^", _task.endTime.year  ,
+            _state == NEW_STATE::END_MONTH    ? "%^%*" : "%^", _task.endTime.month ,
+            _state == NEW_STATE::END_DAY      ? "%^%*" : "%^", _task.endTime.day   ,
+            _state == NEW_STATE::END_HOUR     ? "%^%*" : "%^", _task.endTime.hour  ,
+            _state == NEW_STATE::END_MINUTE   ? "%^%*" : "%^", _task.endTime.minute
+        );
+    }
+    MENU_CASE edit_str(std::string &_str, input::KEYBIND _input)
+    {
+        switch (_input)
+        {
+            case input::KEYBIND::UP:                                                 return MENU_CASE::STAY;
+            case input::KEYBIND::DOWN:                                               return MENU_CASE::STAY;
+            case input::KEYBIND::CONFIRM:                                            return MENU_CASE::STAY;
+
+            case input::KEYBIND::NEXT:                                               return MENU_CASE::NEXT;
+            case input::KEYBIND::BACK:                                               return MENU_CASE::BACK;
+
+            case input::KEYBIND::DELETE:  if (_str.size() > 0) {_str.pop_back();}    return MENU_CASE::STAY;
+            default:                      _str.push_back(static_cast<char>(_input)); return MENU_CASE::STAY;
+        }
+    }
+    MENU_CASE edit_num(int &_num, input::KEYBIND _input)
+    {
+        switch (_input)
+        {
+            case input::KEYBIND::UP:   _num += 1; return MENU_CASE::STAY;
+            case input::KEYBIND::DOWN: _num -= 1; return MENU_CASE::STAY;
+
+            case input::KEYBIND::NEXT: return MENU_CASE::NEXT;
+            case input::KEYBIND::BACK: return MENU_CASE::BACK;
+
+            default:                   return MENU_CASE::STAY;
+        }
+    }
+    MENU_CASE new_name(Task &_task)
+    {
+        while (true)
+        {
+            terminal::clear();
+            print_new_task(_task, NEW_STATE::NAME);
+
+            input::KEYBIND _input = input::read();
+            switch (_input)
+            {
+                case input::KEYBIND::REMOVE:  break;
+                case input::KEYBIND::EDIT:    break;
+                case input::KEYBIND::UP:      break;
+                case input::KEYBIND::DOWN:    break;
+                case input::KEYBIND::NEW:     break;
+                case input::KEYBIND::CONFIRM: break;
+
+                case input::KEYBIND::NEXT:    goto DESCRIPTION;
+                case input::KEYBIND::BACK:    return;
+
+                case input::KEYBIND::DELETE:  /* remove char */ break;
+                default:                      /* add    char */ break;
+            }
+        }
+    }
+    void new_task(Task &_parentTask)
+    {
+        Task _task
+        {
+            .name        = "",
+            .description = "",
+            .status      = 0,
+            .subtasks    = std::vector<Task>(),
+            .startTime   = Time::now(),
+            .endTime     = Time::now(),
+        };
+
+        NEW_STATE _state = NEW_STATE::NAME;
+
+        while (true)
+        {
+            switch (_state)
+            {
+                case NEW_STATE::CANCEL:      return;
+                case NEW_STATE::NAME:        new_name(_task); break;
+                case NEW_STATE::DESCRIPTION: break;
+                case NEW_STATE::START:       break;
+                case NEW_STATE::END:         break;
+                case NEW_STATE::CONFIRM:     break;
+            }
+        }
     }
     void remove_task(Task &_task)
     {
